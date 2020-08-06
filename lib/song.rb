b5e6f6022151be6f5f1715a8e8506e24eb60f0e7
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 15:59:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,16 @@ class Song
   end
   
   def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
   end
   
   def self.create_by_name(name)
+    song = self.new 
+    song.name = name
+    @all << song
+    song
   end
   
   def self.find_by_name(name)
